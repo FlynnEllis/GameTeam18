a439a906c0a7330413aa37f267da588b848c7e6d
jakegolledge <jakegolledge@gmail.com>
2017-10-23 16:41:36 +0100

    added items

--- game/items.py
+++ game/items.py
@@ -2,7 +2,41 @@ class item():
 	def __init__(name,description,mass):
 		self.name = name
 		self.desc = description
-		self. mass = mass
+		self.mass = mass
+		self.price = price
+		self.units = units
 
-beer_bottle_empty = item('an empty beer bottle','description',4.2)
+beer_bottle_empty = item('an empty beer bottle','description',4.2,0,0)
+glass_empty = item('your jacket','description',4.2,0,0)
+keys = item('keys','the keys to your flat',0,0,0)
+wallet = item('wallet','description',0,0,0)
+phone = item('phone','description',0,0,0)
+your_id = item('your id','description',0,0,0)
+drugs = item('drugs','some suspicious white powder',4.2,0,0)
+hipflask = item('hipflask','description',4.2,0,5)
+jacket = item('your jacket','description',4.2,0,0)
+vk = item('a bottle of vk','description',4.2,1,1)
+rum_coke = item('rum and coke','description',4.2,3,2)
+gin_tonic = item('gin and tonic','description',4.2,3,2)
+vodka_shot = item('shot of vodka','description',4.2,2,2)
+jager_bomb = item('Jägerbomb','description',4.2,2,2)
+water = item('glass of water','a glass of water',4.2,0,-1)
+dark_fruits = item('can of dark fruits','description',4.2,3,2)
+red_stripe = item('can of red stripe','description',4.2,3,2)
+brains_pint = item('pint of brains','description',4.2,2,2)
+bloody_mary = item('bloody mary cocktail','description',4.2,4,2)
+sourz_shot = item('shot of sourz','description',4.2,2,1)
+sambuca_shot = item('shot of sambuca','description',4.2,2,2)
+whiskey_coke = item('shot of wiskey','description',4.2,3,2)
+tequila_shot = item('shot of tequila','description',4.2,2,2)
+wine = item('bottle of wine','description',4.2,7,5)
+cheeseburger = item('cheeseburger','description',4.2,1,-1)
+chicken_nuggets = item('chicken nuggets','description',4.2,4,-2)
+mayo_chicken = item('mayo chicken','description',4.2,1,-1)
+wrap_of_the_day = item('wrap of the day','description',4.2,2,-1)
+chips = item('chips','description',4.2,2,-2)
+bigmac = item('big mac','description',4.2,3,-2)
+mcflurry = item('mcflurry','description',4.2,1,0)
+kebab = item('kebab','description',4.2,3,-2)
+
 
