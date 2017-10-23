e7a43c83f81a683b2a22036c0846c382d86a0503
Flynn Ellis <FlynnEllis98@gmail.com>
2017-10-23 15:35:27 +0100

    things changed

--- game/items.py
+++ game/items.py
@@ -1,8 +1,9 @@
 class item():
-	def __init__(name,description,mass):
+	def __init__(name,description,mass,price):
 		self.name = name
 		self.desc = description
-		self. mass = mass
+		self.mass = mass
+		self.price = price
 
-beer_bottle_empty = item('an empty beer bottle','description',4.2)
+beer_bottle_empty = item('an empty beer bottle','description',4.2,0)
 
