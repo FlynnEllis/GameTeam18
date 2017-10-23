3f85a133a1c9651c91d76d2690a6056fb3b6750b
finleylewis4 <flewis553@gmail.com>
2017-10-23 16:35:09 +0100

    template of drinks to be filled out

---
+++ game/drinks_draft.py
@@ -0,0 +1,79 @@
+'''DRINKS:
+(alcohol content measured /100)
+VK
+Rum and coke
+Gin and tonic
+Shots of vodka
+Jägerbombs
+Beer
+Water 
+Dark fruit
+Cider
+Wine
+Brains
+Mojitos
+Margaritas
+Whiskey and coke
+Tequila
+Sambuca
+Sourz
+Bloody mary
+'''
+
+drinks={
+    d_VK : {
+        'description' : 'Fruity beverage availible in a range of flavours',
+        'alc_cont' :
+        }
+    d_rum_coke : {
+        'description' : ''
+        'alc_cont' : ''
+        }
+    d_gin_tonic : {
+        'description' : ''
+        'alc_cont' : ''
+        }
+    d_vodka_shot : {
+        'description' : ''
+        'alc_cont' : ''
+        }
+    d_lager_pint : { #More detail
+        'description' : ''
+        'alc_cont' : ''
+        }
+    d_water : {
+        'description' : ''
+        'alc_cont' : ''
+        }
+    d_dark_fruits : {
+        'description' : ''
+        'alc_cont' : ''
+        }
+    d_mojito_cock : {
+        'description' : ''
+        'alc_cont' : ''
+        }
+    d_margarita_cock : {
+        'description' : ''
+        'alc_cont' : ''
+        }
+    d_whiskey_coke : {
+        'description' : ''
+        'alc_cont' : ''
+        }
+    d_tequila_shot : {
+        'description' : ''
+        'alc_cont' : ''
+        }
+    d_sambuca_shot : {
+        'description' : ''
+        'alc_cont' : ''
+        }
+    d_sourz : {
+        'description' : ''
+        'alc_cont' : ''
+        }
+    d_Rbloody_mary : {
+        'description' : ''
+        'alc_cont' : ''
+        }
