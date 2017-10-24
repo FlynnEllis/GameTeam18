5ec0520d2fe4bae6cd1147f417a206b56697cd88
JoeDinn <joedinn1@gmail.com>
2017-10-24 15:35:07 +0100

    chat stuff

--- game/game.py
+++ game/game.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 from os import system 
-
+import msvcrt
 from map import rooms
 from player import *
 from items import *
@@ -282,7 +282,46 @@ def execute_fight(npc,item):
         print(npc[0].upper() + npc[1:] + ' is not in the room')
     #return npc
  
+def print_chat_options(options,cursor):
+    for index in range(len(options)):
+        if index == cursor:
+            print('{ '+ options[index] +' }')
+        else:
+            print('  ' + options[index])
  
+def navigate_chat_options(options,cursor):
+    system('cls')
+    print_chat_options(options,cursor)
+    key = getkey()
+    while 1:
+        if key == 'up':
+            return navigate_chat_options(options,cursor -1)
+        elif key == 'down':
+            return navigate_chat_options(options,cursor +1)
+        else:
+            return cursor
+        key = getkey()
+ 
+ 
+ 
+def getkey():
+    while True:
+        if msvcrt.kbhit():        
+            c = msvcrt.getch()
+
+            if c in [b'\xe0']:
+                c = msvcrt.getch()
+                return {b'H': 'up', b'P': 'down'}[c]
+
+            elif c  == b'\r':
+                return 'return'
+
+def execute_talk(npc):
+    print(navigate_chat_options(current_room.npcs[npc].lines,0))
+
+def execute_look(item):
+    print('')
+    input()
  
 
 def execute_command(command):
@@ -323,7 +362,17 @@ def execute_command(command):
     	else:
     		execute_fight(command[1],inventory[2])
     		
+    elif command[0] == "talk":
+        if len(command) > 1:
+            execute_talk(command[1])
+        else:
+            print("Talk to who?")
 
+    elif command[0] == 'look':
+        if len(command) > 1:
+            execute_look(command[1])
+        else:
+            print("Look at what?")
 
     else:
         print("This makes no sense.")
