7c6b6c9ed9c01eb340c142fa9facb1796c9d89ac
Flynn Ellis <FlynnEllis98@gmail.com>
2017-10-25 13:24:27 +0100

    added hints to not use bed

--- game/map.py
+++ game/map.py
@@ -36,8 +36,8 @@ room_your_room = room(
     {"back":'pre pre drinks', "bed":'your bed'},
     [wallet, your_id, keys],
     {},
-    """You sit on your bed contemplating your outfit for the night, the walls are shaking from
-    the predrinks occuring next door. It is time to grace them with your presence.
+    """You sit on your bed contemplating your outfit for the night, you could stay here although you feel as if that would be a poor decision. The walls are shaking from
+    the predrinks occuring next door, it would be impossible to sleep. It is time to grace them with your presence.
 
     """)
 
