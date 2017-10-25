7c52390b7e0cfbcc2dc340a842b9d8e4c9cc8134
Flynn Ellis <FlynnEllis98@gmail.com>
2017-10-25 15:36:42 +0100

    fixed definition formatting and added some witty humour

--- game/map.py
+++ game/map.py
@@ -14,12 +14,13 @@ room_pre_drinks =  room(
     {"south": "student union", "east": "tiger tiger", "west": "pryzm", "home": "your room"},
     [], #items
     {}, #npcs
-    """You arrive at pre drinks, the room is full of vodka, testosterone and bad life decisions. 
-    Apart from everyone having a crippling university debt the room seems to be in good spirits. 
-    The music in the room appears to be Grime Time, which you should have expected by now. 
-    From across the room you see your squad and go over to get this party started. 
-    Now do you choose to go hard and drink an alcoholic concoction or stay sober and have a coke?
-    """)
+"""You arrive at pre drinks, the room is full of vodka, testosterone and 
+bad life decisions. Apart from everyone having a crippling university debt
+the room seems to be in good spirits. The music in the room appears to be 
+Grime Time, which you should have expected by now. From across the room you 
+see your squad and go over to get this party started. 
+Now do you choose to go hard and drink an alcoholic concoction
+or stay sober and have a coke?""")
 
 room_pre_pre_drinks = room(
     "pre pre drinks",
@@ -28,64 +29,60 @@ room_pre_pre_drinks = room(
     {'john':npc_john},
 
     """Welcome to the start of the greatest night of your life. 
-    You have received an invitation to the biggest bar crawl in Cardiff which includes such nightlife as the SU, Pryzm and even Tiger Tiger. 
-    Before you leave you should select some belongings that will help you along the way.""")
+You have received an invitation to the biggest bar crawl in Cardiff which 
+includes such nightlife as the SU, Pryzm and even Tiger Tiger. 
+Before you leave you should select some belongings that will 
+help you along the way.""")
     
 room_your_room = room(
     "your room",
     {"back":'pre pre drinks', "bed":'your bed'},
     [wallet, your_id, keys],
     {},
-    """You sit on your bed contemplating your outfit for the night, the walls are shaking from
-    the predrinks occuring next door. It is time to grace them with your presence.
-
-    """)
+    """You sit on your bed contemplating your outfit for the night, the walls are 
+shaking from the predrinks occuring next door. It is time to grace them with 
+your presence.""")
 
 room_bed = room(
     "your bed",
     {},
     [],
     {},
-    """You are now known amongst your friends as the flaker, passive aggressive messages spam your messenger all night. 'enjoying netflix?!?!?!??!'
-    This wasn't your wisest choice.
-
-    """)
+    """You are now known amongst your friends as the flaker, passive aggressive 
+messages spam your messenger all night. 'enjoying netflix?!?!?!??!'
+This wasn't your wisest choice.""")
 
 room_student_union =  room(
     "student union",
     { "tiger": "tiger tiger", "pryzm": "pryzm", "dancefloor": "student union dancefloor", "bar":"student union bar"},
     [],
-
-
-
     {'jill':npc_jill},
-
-
     """You have arrived at the Student Union. 
-    After queueing for what seemed 3 decades you have arrived at the main dance floor. 
-    It appears that a foam party is happening on this particular night. 
-    From across the room you can see half the people trying not to suffocate from the foam and the other half trying to not look like a cardboard cut-out.
-    The music appears to be 80s hits, which you seem to enjoy.
-    Do you make your way to the bar or leave to go to another club?""")
+After queueing for what seemed 3 decades you have arrived at the main 
+dance floor. It appears that a foam party is happening on this particular night. 
+From across the room you can see half the people trying 
+not to suffocate from the foam and the other half trying to not look like a
+cardboard cut-out. The music appears to be 80s hits, which you seem to enjoy.
+Do you make your way to the bar or leave to go to another club?""")
 
 room_student_union_dancefloor =  room(
     "student union dancefloor",
     {"bar": "student union bar","exit":"student union"},
     [],
     {},
-    """The floor is sticky with less fortunate VK's than yours, they have a good owner. You awkwardly sway to the tunes, swigging on your VK occaisionally to blend in.
-    You are not quite sure why anyone drinks VK's.
-    """)
+    """The floor is sticky with less fortunate VK's than yours, they have a good owner. 
+You awkwardly sway to the tunes, swigging on your VK occaisionally to blend in.
+You are not quite sure why anyone drinks VK's.""")
 
 room_student_union_bar =  room(
     "student union bar",
     {"dancefloor": "student union dancefloor", "exit":"student union"},
     [],
     {'bartender': npc_su_bar},
-    """After queing for at least half an hour you make it to the bar. As the pressure of the people pushing on you grows
-    you manage to shout VK breathlessly at the bartender. He asks what colour and you desperately gesture at the fridge.
-    Luckily VK's only only cost 1 tonight. Perfect.
-    """)
+    """After queing for at least half an hour you make it to the bar. As the pressure
+of the people pushing on you grows you manage to shout VK breathlessly at the 
+bartender. He asks what colour and you desperately gesture at the fridge.
+Luckily VK's only only cost 1 tonight. Perfect.""")
 
 room_tiger_tiger =  room(
     "tiger tiger",
@@ -93,8 +90,10 @@ room_tiger_tiger =  room(
     [],
     {},
     """You arrive at the ever hyped but always dissapointing Tiger Tiger. 
-    The queue is as short as your life expectancy so you enter straight away. 
-    The atmosphere seems to have a sort of life to it although what that life is you are never sure. You can choose to enter this uncommon occurrence or leave for yet another cesspool. """)
+The queue is as short as your life expectancy so you enter straight away. 
+The atmosphere seems to have a sort of life to it although
+what that life is you are never sure. You can choose to enter this uncommon
+occurrence or leave for yet another cesspool. """)
 
 room_tiger_tiger_main =  room(
     "tiger tiger main",
@@ -102,8 +101,9 @@ room_tiger_tiger_main =  room(
     [],
     {},
     """You are in the main room of tiger tiger!
-    Much to your surprise the normally barren and empty dancefloor is full of people, 
-    however floors are a bit sticky so dancing will require a bit more effort than you would like.
+Much to your surprise the normally barren and empty dancefloor
+is full of people, however floors are a bit sticky so dancing will require
+a bit more effort than you would like.
 
     """)
 
@@ -112,9 +112,8 @@ room_tiger_tiger_bar =  room(
     {"exit":"tiger tiger", "south": "tiger tiger main", "tiger2":"tiger tiger second"},
     [],
     {'bartender': npc_tiger_tiger_bar},
-    """
-
-    """)
+    """It appears you can buy numerous alcoholic beverages here. Perhaps
+to your detriment.""")
 
 room_tiger_tiger_second =  room(
     "tiger tiger second",
@@ -122,18 +121,17 @@ room_tiger_tiger_second =  room(
     [],
     {},
     """You enter the second room in tiger tiger.
-    its kinda like the other one but the floors arent as sticky this time.
-
-    """)
+its kinda like the other one but the floors arent as sticky this time.""")
 
 room_tiger_tiger_toilet =  room(
     "tiger tiger toilet",
     {"tiger2": "tiger tiger second", "south":"tiger tiger main","bar":"tiger tiger bar"},
     [],
     {},
-    """
-
-    """)
+    """You contemplate why club toilets are designed so badly, you soon realise
+that like IKEA the layout is designed to send you past 'Freshen up guy'. 
+After refusing a selection of gum and manly scents 
+you make it to the toilet.""")
 
 
 room_pryzm =  room(
@@ -142,28 +140,30 @@ room_pryzm =  room(
     [],
     {},
     """You arrive at Pryzm. 
-    The queue was so long you started to question your life decisions. 
-    As you finally get into the club, you see two things: 
-    The club is packed and there is a lot of creepy guys grabbing girls everywhere without consent. 
-    If you would like to join in and lose all sense of pride and eventually enter the depths of hell, proceed and enter to the bar or leave for another club.""")
+The queue was so long you started to question your life decisions. 
+As you finally get into the club, you see two things: 
+The club is packed and there is a lot of creepy guys looking at girls. 
+If you would like to join in and lose all sense of pride 
+and eventually enter the depths of hell, proceed and enter to the bar 
+or leave for another club.""")
 
 room_pryzm_bar =  room(
     "pryzm bar",
     {"south": "pryzm main", "disco":"pryzm disco", "toilet":"pryzm toilet", "exit":"pryzm"},
     [],
     {'bartender': npc_pryzm_bar},
-    """You hope you brought enough gold bullion with you as you approach the pryzm bar. The bartender spins bottles around fancily whilst pouring the
-    last order. I guess spirits are the order of the night here.
-    """)
+    """You hope you brought enough gold bullion with you as you approach the pryzm bar. 
+The bartender spins bottles around fancily whilst pouring the last order. 
+I guess spirits are the order of the night here.""")
 
 room_pryzm_main =  room(
     "pryzm main",
     {"disco": "pryzm disco", "bar":"pryzm bar", "toilet":"pryzm toilet","exit":"pryzm"},
     [],
     {'Barry Chuckle' : npc_Chuckle_1, 'Paul Chuckle' : npc_Chuckle_2},
-    """Welcome to the main floor where the bass is giving you heart palputations and the lights elevating you to a new plane of being,
-    in the corner you spot two odd looking men with moustaches taking pictures with everyone.
-    """)
+    """Welcome to the main floor where the bass is giving you heart palputations 
+and the lights elevating you to a new plane of being, 
+in the corner you spot two odd looking men with moustaches taking pictures with everyone.""")
 
 room_pryzm_disco =  room(
     "pryzm disco",
@@ -171,16 +171,16 @@ room_pryzm_disco =  room(
     [],
     {},
     """The sound of High School Musical greets your ears as you enter the disco room, you take one last deep breath before proceeding to belt
-    out the lyrics to the best of your ability. Perhaps you should work on those high notes.
-
-    """)
+out the lyrics to the best of your ability. Perhaps you should work on those 
+high notes.""")
 
 room_pryzm_toilet=  room(
     "pryzm toilet",
     {"smoke":"pryzm smoke", "north": "pryzm main", "exit":"pryzm"},
     [],
     {},
-    """The smell of piss and other delights greets you as you walk into the toilets. They are packed as usual and you have to wait for your time to shine.
+    """The smell of piss and other delights greets you as you walk into the toilets. 
+They are packed as usual and you have to wait for your time to shine.
     """)
 
 
@@ -189,10 +189,9 @@ room_pryzm_smoking_area =  room(
     {"toilets": "pryzm toilet", "exit":"pryzm"},
     [],
     {},
-    """You walk into the smoking area and reach for your pockets for a cigarette, it is then you remember that smoking shortens your life expectancy by ten years,
-    you won't be smoking today.
-
-    """)
+    """You walk into the smoking area and reach for your pockets for a cigarette, 
+it is then you remember that smoking shortens your life expectancy by ten years, you won't be smoking today.
+""")
 
 room_welsh_pub =  room(
     "welsh pub",
@@ -200,11 +199,10 @@ room_welsh_pub =  room(
     [],
     {},
     """You have somehow ended up in a welsh pub. The delights of Delilah fill your ears, along with the cacophony of
-    a room full of balding middle aged men singing along, they all seem to be glaring at you.
-    They know you dont belong here. 
-    You know you dont belong here.
-
-    """)
+a room full of balding middle aged men singing along, they all seem to be glaring at you.
+They know you dont belong here. 
+You know you dont belong here.
+""")
 
 room_pub_bar =  room(
     "pub bar",
@@ -212,10 +210,9 @@ room_pub_bar =  room(
     [],
     {'bartender': npc_welsh_bar},
     """It looks like a pint of Brains is the favourite drink here. Maybe it will help you blend in.
-    The welsh national anthem comes on and you try your best to move your mouth to match the funny sounds whilst you wait
-    for your order.
-
-    """)
+The welsh national anthem comes on and you try your best to move your mouth to match the funny sounds
+whilst you wait for your order.
+""")
 
 room_river = room(
     "river",
@@ -223,8 +220,7 @@ room_river = room(
     [],
     {},
     """It appears whilst drunk you stumbled and fell in the river Taff, the water seeps into your clothes as your feet touch a number of foreign objects.
-    Was that a shopping trolley?
-    """)
+Was that a shopping trolley?""")
 
 
 #room_glam =  room(
